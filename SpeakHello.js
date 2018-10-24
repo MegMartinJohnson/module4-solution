d80a2486e3f87c2689dc7be70af76760a84fe3ba
MegMartinJohnson <43734870+MegMartinJohnson@users.noreply.github.com>
2018-10-24 15:44:23 -0400

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -8,7 +8,9 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+(function () {
+  
+  var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
@@ -22,3 +24,6 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+  
+}) ();
